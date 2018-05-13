e7512854987bbb6f8f5ca2110960b78424f10347
Shun Yao <ysmiracle@hotmail.com>
2018-05-13 14:51:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  let charge;
+  if (ride <= 400) {
+    charge = 'This one is on me!';
+  } else if (ride > 2500) {
+      charge = 'No can do.';
+  } else if (ride > 2000) {
+      charge = 'I will gladly take your thirty bucks.';
+  }
+  return charge;
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  let charm;
+  switch (tip) {
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = 'Bye.';
+  }
+  return charm;
+}
